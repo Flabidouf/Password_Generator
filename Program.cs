5f560aaf60ff971027044578bb0fc2a01fc6b8cb
Marius <mariuswaseige@protonmail.com>
2023-10-03 15:04:00 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using FormationCS;
+
+namespace Password_Generator
+{
+    class Program
+    {
+        /*enum e_Operator //Pas besoin de ça, la fonction AskNumberBetween fait la même chose
+        {
+            MINUSCULES = 1,
+            MIN_MAJ = 2,
+            MIN_MAJ_N = 3, 
+            MIN_MAJ_N_X = 4
+        }*/
+       
+        static void Main(string[] args)
+        {
+            
+            int nb_Password = tools.AskNumberPositive("How many passwords do you need ?\n" +
+                                                      "Your choice : ");
+            
+            int passwordLength = tools.AskNumberPositive("Length of password : ");
+            
+            Console.WriteLine();
+
+           
+            // On réutilise la fonction AskNumberBetween en donnant à son paramètre précédemment "question",
+            // une string avec le message que l'on souhaite avant d'utiliser la fonction.
+            int chosenAlphabet = tools.AskNumberBetween("You want a password with :\n" + 
+                                                        "1 - Lowercase\n" +
+                                                        "2 - Lowercase and uppercase\n" + 
+                                                        "3 - Lowercase, uppercase and numbers\n" + 
+                                                        "4 - Lowercase, uppercase, numbers and specials\n" + 
+                                                        "Your choice : ",1, 4);
+
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            string uppercase = lowercase.ToUpper();
+            string numbers = "123456789";
+            string specialChars = "#&%$-+!";
+            string alphabet = lowercase + uppercase + numbers + specialChars;
+          
+            string passWord = "";       
+
+            Random rand = new Random();
+
+            // Pas d'accolades pcq une seule ligne de code par if 
+            if (chosenAlphabet == 1)
+                alphabet = lowercase;
+            else if (chosenAlphabet == 2)
+                alphabet = lowercase + uppercase;
+            else if (chosenAlphabet == 3)
+                alphabet = lowercase + uppercase + numbers;
+            else
+                alphabet = lowercase + uppercase + numbers + specialChars;
+            
+            
+            int alphabetLength = alphabet.Length;
+
+            for (int j = 0; j < nb_Password; j++)
+            {
+                passWord = "";
+                for (int i = 0; i < passwordLength; i++)
+                {
+                    int index = rand.Next(0, alphabetLength); // Pas de l-1 pcq dernière valeur exclue ave .Next
+                    passWord += alphabet[index];
+                    
+                }
+
+                Console.WriteLine("Password = " + passWord);
+            }
+        }
+    }
+}
+
---
+++ tools.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+
+
+    namespace FormationCS
+    {
+        public class tools
+        {
+            
+           
+                public static int AskNumberPositive(string question)
+                {
+                   /* int positiveNumber = AskNumber(question);
+
+                    if (positiveNumber > 0)
+                    {
+                        return positiveNumber;
+                    }
+                    Console.WriteLine("Error, number must be positive");
+
+                    return AskNumberPositive(question);*/
+
+                   // ou :
+
+                     return AskNumberBetween(question, 1, int.MaxValue, "Error: The number must be positive");
+                }
+
+
+
+                public static int AskNumberBetween(string question, int min, int max, string personnalisedErrorMsg = null)
+                { // On ajoute un paramètre personnalisedErrorMsg afin d'ajouter un string
+                  // en paramètre quand on reprend la fonction ailleurs.
+                    int number = AskNumber(question);
+
+                    if ((number >= min) && (number <= max))
+                    {
+                        return number;
+                    }
+
+                    if (personnalisedErrorMsg == null)
+                    {
+                        Console.WriteLine("Error, number must be between " + min + " and " + max);
+                    }
+                    else
+                    {
+                        Console.WriteLine(personnalisedErrorMsg);
+                    }
+
+                    Console.WriteLine();
+                    
+                    return AskNumberBetween(question, min, max, personnalisedErrorMsg); 
+                    // Demander à Max ce que fait le return ici, le return retourne la fonction vers où ? 
+                    // Pourquoi fait-on ça ?
+                    
+                }
+                
+                
+
+                static int AskNumber(string question)
+                {
+
+                    while (true)
+                    {
+                        Console.WriteLine(question);
+                        string answer = Console.ReadLine();
+
+                        try
+                        {
+                            int answerInt = int.Parse(answer);
+                            return answerInt;
+
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Error, enter a valid number.");
+                            Console.WriteLine();
+                        }
+
+                    }
+                }
+            
+        }
+    }
+
